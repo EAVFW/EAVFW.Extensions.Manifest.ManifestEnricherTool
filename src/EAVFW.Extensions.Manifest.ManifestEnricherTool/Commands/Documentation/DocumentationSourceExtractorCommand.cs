eb98cbba640687fb2d3015a602ca27313390f795
Thyge Skødt Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2023-11-29 13:59:38 +0100

    feat: ProbePattern or list of assemblies replace configuration and dotnet version to find assemblies

--- src/EAVFW.Extensions.Manifest.ManifestEnricherTool/Commands/Documentation/DocumentationSourceExtractorCommand.cs
+++ src/EAVFW.Extensions.Manifest.ManifestEnricherTool/Commands/Documentation/DocumentationSourceExtractorCommand.cs
@@ -8,15 +8,16 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using EAVFW.Extensions.Docs.Extractor;
+using Microsoft.Extensions.FileSystemGlobbing;
 
 namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
 {
     /*
      * There are many parameters to the command and generating source for both plugin and wizard requires the tool to be
      * executed twice. Alternatively, the "configuration" could be done using a configuration, which would provide all
-     * necessary parameters. Both it would be more rigid and could not easily be changed in a pipeline or other. 
+     * necessary parameters. Both it would be more rigid and could not easily be changed in a pipeline or other.
      */
-    
+
     public class DocumentationSourceExtractorCommand : Command
     {
         private readonly IDocumentLogic _documentLogic;
@@ -34,7 +35,7 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
         [Alias("-p")]
         [Alias("--probing-pattern")]
         [Description("Path pattern used to probe for assemblies, supporting glob patterns")]
-        public DirectoryInfo ProbePathOption { get; set; }
+        public IEnumerable<string> ProbePathOption { get; set; }
 
         [Alias("-o")]
         [Alias("--output")]
@@ -46,13 +47,19 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
         [Description("What kind of documentation source should be extracted")]
         public Targets Target { get; set; }
 
+        [Alias("-d")]
+        [Alias("--debug")]
+        [Description("Enable debug output")]
+        public bool Debug { get; set; } = false;
+
         public enum Targets
         {
             Plugins,
             Wizards
         }
 
-        public DocumentationSourceExtractorCommand(IDocumentLogic documentLogic) : base("extract", "Extract documentation source")
+        public DocumentationSourceExtractorCommand(IDocumentLogic documentLogic) : base("extract",
+            "Extract documentation source")
         {
             _documentLogic = documentLogic ?? throw new ArgumentNullException(nameof(documentLogic));
             Handler = COmmandExtensions.Create(this, Array.Empty<Command>(), Run);
@@ -69,6 +76,37 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
             if (!AssemblyPathOption.Exists)
             {
                 Console.WriteLine("Assembly does not exists");
+                return 1;
+            }
+
+            if (ProbePathOption.Count() == 1 && ProbePathOption.First().Contains('*'))
+            {
+                DebugMsg("No assemblies in list, probing...");
+
+                var probePath = new DirectoryInfo(ProbePathOption.First());
+             
+                var matcher = new Matcher();
+                var basePath = probePath.Parent;
+            
+                if (probePath.FullName.Contains("**"))
+                {
+                    basePath = new DirectoryInfo(probePath.FullName.Split("**").First());
+                    matcher.AddInclude(probePath.FullName[basePath.FullName.Length..]);
+                }
+                else if (probePath.Name.Contains("."))
+                {
+                    matcher.AddInclude(probePath.Name);
+                }
+            
+                ProbePathOption = matcher.GetResultsInFullPath(basePath.FullName).ToList();
+
+            }
+            
+            DebugMsg($"Found {ProbePathOption.Count()} assemblies");
+
+            if (!ProbePathOption.Any())
+            {
+                Console.WriteLine("Did not find any assemblies");
                 return 1;
             }
 
@@ -92,15 +130,13 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
             }
 
             var entityDefinitions = _documentLogic.ExtractWizardDocumentation(
-                ManifestPathOption,
-                new PluginInfo(ProbePathOption,
-                    AssemblyPathOption));
+                ManifestPathOption, AssemblyPathOption, ProbePathOption.ToArray());
 
             var basePath = new DirectoryInfo(CalculateFullPath("wizards"));
 
-            if(!basePath.Exists)
+            if (!basePath.Exists)
                 basePath.Create();
-            
+
             foreach (var (key, value) in entityDefinitions)
             {
                 var fileName = Path.Combine(basePath.FullName, $"{key}.json");
@@ -119,9 +155,9 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
         private async Task<int> GeneratePluginSource()
         {
             var plugins = _documentLogic
-                .ExtractPluginDocumentation(new PluginInfo(ProbePathOption, AssemblyPathOption))
+                .ExtractPluginDocumentation(AssemblyPathOption, ProbePathOption.ToArray())
                 .ToArray();
-            
+
             var jsonString = JsonSerializer.Serialize(plugins, new JsonSerializerOptions
             {
                 WriteIndented = true
@@ -154,6 +190,12 @@ namespace EAVFW.Extensions.Manifest.ManifestEnricherTool.Commands.Documentation
                 missing.Add(nameof(ProbePathOption));
 
             return false;
+        }
+
+        private void DebugMsg(string msg)
+        {
+            if (Debug)
+                Console.WriteLine(msg);
         }
     }
 }
